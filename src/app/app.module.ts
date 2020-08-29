8219f0b593897d2d5180ea854a5b27d7889b326d
SKevinPerera <sumalkevinthushantha@gmail.com>
2020-08-29 23:52:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
