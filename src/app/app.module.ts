b440793c4b756fda606d952dd223fca1d38155b7
SKevinPerera <kevin.thushantha@gmail.com>
2022-01-30 23:29:42 +0530

    Commiting crousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
